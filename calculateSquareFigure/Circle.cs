3eea26c8b29a7ace4c8cacf4bd9ec10c62f6abdd
Шамиль Валиулин <shaman.valius@gmail.com>
2022-07-17 13:36:51 +0500

    Add geomtric figure - Circle

---
+++ calculateSquareFigure/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculateSquareFigure
+{
+    public class Circle : IGeometricFIgure
+    {
+        private double radius;
+
+        public double Radius { get => radius; set => radius = value; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Square()
+        {
+            return Math.Round(3.14f * Math.Pow(Radius, 2), 1);
+        }
+    }
+}
